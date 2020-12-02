0bf11fdd2b00d3b59c97280f0b7ace9ce34fc78c
Dominic852 <43085764+Dominic852@users.noreply.github.com>
2020-12-02 15:43:47 +0800

    [SpringBoot][Dominic Leung] feat: add addEmployee function to add a employee

--- src/main/java/com/thoughtworks/springbootemployee/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/Employee.java
@@ -7,13 +7,6 @@ public class Employee {
     private String gender;
     private int salary;
 
-    public Employee(int id, String name, int age, String gender, int salary) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.salary = salary;
-    }
 
     public int getId() {
         return id;
@@ -33,5 +26,25 @@ public class Employee {
 
     public String getName() {
         return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
     }
 }
--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,9 +1,7 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,14 +9,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
-    List<Employee> employees=new ArrayList<>();
+    List<Employee> employees = new ArrayList<>();
 
-    public EmployeeController() {
-        this.employees.add(new Employee(1,"abc",10,"Male",10000) );
-    }
 
     @GetMapping
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         return this.employees;
+    }
+
+    @PostMapping
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employees.add(employee);
+        return employee;
     }
 }
