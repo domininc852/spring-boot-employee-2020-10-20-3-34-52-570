f6bd96f6bc1b1505c2992c84cbf26eb1bc58328f
Dominic852 <43085764+Dominic852@users.noreply.github.com>
2020-12-02 23:22:15 +0800

    [SpringBoot][Dominic Leung] refactor: delete unused import

--- src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
