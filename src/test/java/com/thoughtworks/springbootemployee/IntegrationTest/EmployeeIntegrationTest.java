af9d9f6dbe2c50b393bfc6313db1d7c027a5c673
Dominic852 <43085764+Dominic852@users.noreply.github.com>
2020-12-03 18:06:11 +0800

    [SpringBoot][Dominic Leung] test: add integration test for employee: getAll, post, put and delete

---
+++ src/test/java/com/thoughtworks/springbootemployee/IntegrationTest/EmployeeIntegrationTest.java
@@ -0,0 +1,140 @@
+package com.thoughtworks.springbootemployee.IntegrationTest;
+
+import com.thoughtworks.springbootemployee.Employee;
+import com.thoughtworks.springbootemployee.repositories.EmployeeRepository;
+import org.bson.types.ObjectId;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeIntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @AfterEach
+    void tearDown() {
+        employeeRepository.deleteAll();
+    }
+
+    @Test
+    public void should_return_all_employees_when_get_all_given_employees() throws Exception {
+
+        //given
+        Employee employee = new Employee("bar", 20, "Female", 120);
+        employeeRepository.save(employee);
+        //when
+        //then
+        mockMvc.perform(get("/employees")).
+                andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isString())
+                .andExpect(jsonPath("$[0].name").value("bar"))
+                .andExpect(jsonPath("$[0].age").value(20))
+                .andExpect(jsonPath("$[0].gender").value("Female"))
+                .andExpect(jsonPath("$[0].salary").value(120));
+    }
+
+    @Test
+    public void should_return_employee_when_create_given_employee() throws Exception {
+        //given
+        String employeeAsJson = "{\n" +
+                "        \"name\": \"foobar\",\n" +
+                "        \"age\": 3,\n" +
+                "        \"gender\": null,\n" +
+                "        \"salary\": 4\n" +
+                "    }";
+        //when
+        //then
+        mockMvc
+                .perform(post("/employees")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(employeeAsJson)
+                )
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isString())
+                .andExpect(jsonPath("$.name").value("foobar"))
+                .andExpect(jsonPath("$.age").value(3))
+                .andExpect(jsonPath("$.salary").value(4));
+
+    }
+
+    @Test
+    public void should_return_updated_employee_when_update_employee_given_an_employee() throws Exception {
+        //given
+        Employee employee = employeeRepository.save(new Employee("bar", 20, "Female", 120));
+        String employeeToString = " {\n" +
+                "            \"name\": \"bar\",\n" +
+                "            \"age\": 20,\n" +
+                "            \"gender\": \"Female\",\n" +
+                "            \"salary\": 120\n" +
+                "    }";
+        //when
+
+        //then
+        mockMvc.perform(put("/employees/" + employee.getId()).contentType(MediaType.APPLICATION_JSON).content(employeeToString))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(employee.getId()))
+                .andExpect(jsonPath("$.name").value("bar"))
+                .andExpect(jsonPath("$.age").value(20))
+                .andExpect(jsonPath("$.salary").value(120))
+                .andExpect(jsonPath("$.gender").value("Female"));
+    }
+    @Test
+    public void should_return_404_error_when_update_employee_given_an_invalid_employeeID() throws Exception {
+        //given
+        Employee employee = employeeRepository.save(new Employee("bar", 20, "Female", 120));
+        String employeeToString = " {\n" +
+                "            \"name\": \"bar\",\n" +
+                "            \"age\": 20,\n" +
+                "            \"gender\": \"Female\",\n" +
+                "            \"salary\": 120\n" +
+                "    }";
+        ObjectId fakeID = new ObjectId();
+        //when
+
+        //then
+        mockMvc.perform(put("/employees/" + fakeID).contentType(MediaType.APPLICATION_JSON).content(employeeToString))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void should_delete_a_employee_when_delete_employee_given_an_employeeID() throws Exception {
+        //given
+        Employee employee = employeeRepository.save(new Employee("bar", 20, "Female", 120));
+
+        //when
+
+        //then
+        mockMvc.perform(delete("/employees/" + employee.getId()))
+                .andExpect(status().isOk());
+        assertTrue(employeeRepository.findById(employee.getId()).isEmpty());
+    }
+    @Test
+    public void should_return_404_error__when_delete_employee_given_an_invalid_employeeID() throws Exception {
+        //given
+        Employee employee = employeeRepository.save(new Employee("bar", 20, "Female", 120));
+        ObjectId fakeID= new ObjectId();
+        //when
+
+        //then
+        mockMvc.perform(delete("/employees/"+fakeID))
+                .andExpect(status().isNotFound());
+    }
+
+}
---
+++ src/test/java/com/thoughtworks/springbootemployee/Service/CompanyServiceTest.java
@@ -0,0 +1,140 @@
+//package com.thoughtworks.springbootemployee.Service;
+//
+//import com.thoughtworks.springbootemployee.Company;
+//import com.thoughtworks.springbootemployee.Employee;
+//import com.thoughtworks.springbootemployee.services.CompanyService;
+//import org.junit.Test;
+//import org.mockito.ArgumentCaptor;
+//import org.mockito.Mockito;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.mockito.Mockito.times;
+//import static org.mockito.Mockito.when;
+//
+//public class CompanyServiceTest {
+//    @Test
+//    public void should_company_service_return_all_companies_when_get_all_given_companies() {
+//        //given
+//        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
+//        CompanyService companyService = new CompanyService(companyRepository);
+//        List<Employee> employees = new ArrayList<>();
+//        employees.add(new Employee(1, "test", 18, "Male", 10000));
+//        List<Company> companies = new ArrayList<>();
+//        companies.add(new Company("ABC", 1, employees, 1));
+//        Mockito.when(companyRepository.getAll()).thenReturn(companies);
+//
+//        //when
+//        List<Company> actualCompanies = companyService.getAll();
+//
+//        //then
+//        assertEquals(companies, actualCompanies);
+//    }
+//
+//    @Test
+//    public void should_return_created_company_when_create_company_given_a_company() {
+//        //given
+//        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
+//        CompanyService companyService = new CompanyService(companyRepository);
+//        List<Employee> employees = new ArrayList<>();
+//        employees.add(new Employee(1, "test", 18, "Male", 10000));
+//        Company company = new Company("ABC", 1, employees, 1);
+//        Mockito.when(companyRepository.save(company)).thenReturn(company);
+//
+//        //when
+//        companyService.save(company);
+//        final ArgumentCaptor<Company> companyArgumentCaptor = ArgumentCaptor.forClass(Company.class);
+//        Mockito.verify(companyRepository, times(1)).save(companyArgumentCaptor.capture());
+//        //then
+//        final Company actual = companyArgumentCaptor.getValue();
+//        assertEquals(company, actual);
+//    }
+//
+//    @Test
+//    public void should_return_updated_company_when_update_company_given_a_company_companyID() {
+//        //given
+//        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
+//        CompanyService companyService = new CompanyService(companyRepository);
+//        List<Employee> employees = new ArrayList<>();
+//        employees.add(new Employee(1, "test", 18, "Male", 10000));
+//        Company company = new Company("ABC", 1, employees, 1);
+//        Mockito.when(companyRepository.update(1, company)).thenReturn(company);
+//
+//        //when
+//        Company actual = companyService.update(1, company);
+//        //then
+//        assertEquals(company, actual);
+//    }
+//
+//    @Test
+//    public void should_delete_company_when_delete_company_given_companyID() {
+//        //given
+//        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
+//        CompanyService companyService = new CompanyService(companyRepository);
+//        List<Employee> employees = new ArrayList<>();
+//        employees.add(new Employee(1, "test", 18, "Male", 10000));
+//        Company company = new Company("ABC", 1, employees, 1);
+//
+//        //when
+//        companyService.delete(1);
+//        //then
+//        Mockito.verify(companyRepository, times(1)).delete(1);
+//    }
+//
+//    @Test
+//    public void should_return_company_when_get_company_with_id_given_companyID() {
+//        //given
+//        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
+//        CompanyService companyService = new CompanyService(companyRepository);
+//        List<Employee> employees = new ArrayList<>();
+//        employees.add(new Employee(1, "test", 18, "Male", 10000));
+//        Company company = new Company("ABC", 1, employees, 1);
+//        when(companyRepository.getCompanyByID(1)).thenReturn(company);
+//        //when
+//        Company actual = companyService.getCompanyByID(1);
+//        //then
+//        assertEquals(company, actual);
+//    }
+//
+//    @Test
+//    public void should_return_employees_when_get_employees_with_company_id_given_companyID() {
+//        //given
+//        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
+//        CompanyService companyService = new CompanyService(companyRepository);
+//        List<Employee> employees = new ArrayList<>();
+//        employees.add(new Employee(1, "test", 18, "Male", 10000));
+//        Company company = new Company("ABC", 1, employees, 1);
+//        when(companyRepository.getEmployeesByCompanyID(1)).thenReturn(employees);
+//        //when
+//        List<Employee> actual = companyService.getEmployeesByCompanyID(1);
+//        //then
+//        assertEquals(employees, actual);
+//    }
+//
+//    @Test
+//    public void should_return_companies_when_get_companies_given_specific_page_and_page_size() {
+//        //given
+//        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
+//        CompanyService companyService = new CompanyService(companyRepository);
+//        List<Employee> employees1 = new ArrayList<>();
+//        employees1.add(new Employee(1, "test1", 18, "Male", 10000));
+//        Company company1 = new Company("ABC", 1, employees1, 1);
+//        List<Employee> employees2 = new ArrayList<>();
+//        employees2.add(new Employee(2, "test2", 18, "Male", 10000));
+//        Company company2 = new Company("EFG", 1, employees2, 2);
+//        List<Employee> employees3 = new ArrayList<>();
+//        employees3.add(new Employee(3, "test3", 18, "Male", 10000));
+//        Company company3 = new Company("HIJ", 1, employees3, 3);
+//
+//        List<Company> companies = new ArrayList<>();
+//        companies.add(company3);
+//        when(companyRepository.getCompaniesByPageAndPageSize(2, 2)).thenReturn(companies);
+//        //when
+//        List<Company> actual = companyService.getCompaniesByPageAndPageSize(2, 2);
+//        //then
+//        assertEquals(companies, actual);
+//
+//    }
+//}
